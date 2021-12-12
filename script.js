21b61eaec8f0eff0751ac13417f6b7a44ff569de
IhorMykhno <73365101+IhorMykhno@users.noreply.github.com>
2021-12-12 23:19:38 +0200

    Add files via upload
    final

--- script.js
+++ script.js
@@ -1,29 +1,100 @@
 const rates = {};
-const elementUSD = document.querySelector('[data-value="USD"]')
-const elementEUR = document.querySelector('[data-value="EUR"]')
-const elementRUB = document.querySelector('[data-value="RUB"]')
+const currency = {};
 
 const input = document.querySelector('#input')
 const result = document.querySelector('#result')
-const select  = document.querySelector('#select')
+const selectInput = document.querySelector('#select-input')
+const select  = document.querySelector('#select-result')
+const todayDay = document.querySelector('[currency="today"]')
+
 getCurrencies();
 
 async function getCurrencies(){
-    const response = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
-    const data = await response.json()
-    const result = await data;
-
-    rates.USD = result.find(r => r.cc == "USD");
-    rates.EUR = result.find(r => r.cc == "EUR");
-    rates.RUB = result.find(r => r.cc == "RUB");
+    const responce = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+    const data = await responce.json()
+    const currency = await data;
+    currency.forEach(r => {
+        rates[r.cc] = {title: r.txt, rate: r.rate};
+    });
     
-    elementUSD.textContent = rates.USD.rate.toFixed(2);
-    elementEUR.textContent = rates.EUR.rate.toFixed(2);
-    elementRUB.textContent = rates.RUB.rate.toFixed(2);
+    addTodayDate();
+    printTotalCurrency(rates);
+    addCurrencyToSelect(currency);
+    converValue(currency)
 } 
+
+function addTodayDate(){
+    let date = new Date();
+    todayDay.append(date.toLocaleDateString("ru-RU"))
+}
+async function printTotalCurrency(rates){
+    const valutes = {};
+    const responce = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+    const res = await responce.json();
+
+    valutes.USD = res.Valute.USD;
+    valutes.EUR = res.Valute.EUR;
+    valutes.GBP = res.Valute.GBP;
+
+    const elementUSD = document.querySelector('[data-value="USD"]')
+    const elementEUR = document.querySelector('[data-value="EUR"]')
+    const elementGBP = document.querySelector('[data-value="GBP"]')
+    
+    elementUSD.textContent = valutes.USD.Value.toFixed(2);
+    elementUSD.appendChild(isUp(valutes.USD));
+    
+    elementEUR.textContent = valutes.EUR.Value.toFixed(2);
+    elementEUR.appendChild(isUp(valutes.EUR));
+
+    elementGBP.textContent = valutes.GBP.Value.toFixed(2);
+    elementGBP.appendChild(isUp(valutes.GBP));
+}
+function isUp(valute){
+    let span;
+    if(valute.Value < valute.Previous){
+        span = document.createElement('span');
+        span.textContent = '▼';    
+        span.className =  'down';
+    }
+    else {
+        span = document.createElement('span');
+        span.textContent = '▲';    
+        span.className =  'up';
+    }
+    return span;
+}
+function addCurrencyToSelect(currency){
+    currency.forEach(c => {
+        let option = document.createElement('option');
+        option.value = c.cc
+        option.innerHTML = `${c.cc} - ${c.txt}`;
+        select.appendChild(option);
+    })   
+    currency.forEach(c => {
+        let option = document.createElement('option');
+        option.value = c.cc
+        option.innerHTML = `${c.cc} - ${c.txt}`;
+        selectInput.appendChild(option);
+    })   
+}
 input.oninput = converValue;
 select.oninput = converValue;
+selectInput.oninput = converValue;
 
-function converValue(){
-    result.value = (parseFloat(input.value) / rates[select.value].rate).toFixed(2)     
+function converValue(){  
+    if(selectInput.value !== 'UAH'){
+        if(select.value === 'UAH'){
+            result.value = (parseFloat(input.value) * rates[selectInput.value].rate).toFixed(2)
+        }
+        else{
+            let toUAH = (parseFloat(input.value) * rates[selectInput.value].rate);
+            result.value = (toUAH / rates[select.value].rate).toFixed(2)
+        }
+    }
+    else {
+        if(select.value === selectInput.value)
+            result.value = input.value
+        else        
+            result.value = (parseFloat(input.value) / rates[select.value].rate).toFixed(2)
+    }
 }
